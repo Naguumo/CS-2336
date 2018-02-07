4d31e7ad56d77392ea4c27e7cd30739bf157f7c6
Ishaan <Ishaan@10.23.78.247>
2018-02-07 15:16:13 -0600

    Project 2 Corrections

--- src/Calculator/ComplexNumber.java
+++ src/Calculator/ComplexNumber.java
@@ -19,5 +19,5 @@ public class ComplexNumber extends Number
                 String.format("%.2f%s%.2fi",nReal, (nImaginary > 0)?"+":"-", Math.abs(nImaginary));} //Corrects to proper Operation Symbol
     @Override
     public boolean equals(Object a){return (a instanceof ComplexNumber)? nImaginary == ((ComplexNumber)a).getImaginaryNumber() && nReal == ((ComplexNumber)a).getNumber() : false;}
-    public int compareTo(ComplexNumber a){return (int)(Math.sqrt(Math.pow(nReal,2)+Math.pow(nImaginary,2))-Math.sqrt(Math.pow(a.getNumber(),2)+Math.pow(a.getImaginaryNumber(),2)));}
+    public int compareTo(Object a){return (a instanceof ComplexNumber) ? (int)(Math.sqrt(Math.pow(nReal,2)+Math.pow(nImaginary,2))-Math.sqrt(Math.pow(((ComplexNumber)a).getNumber(),2)+Math.pow(((ComplexNumber)a).getImaginaryNumber(),2))) : 0;}
 }
